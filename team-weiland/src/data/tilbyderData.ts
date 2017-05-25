d986bc728ffad93198109edc0c74652d8de02ee5
FridrikOlafsson <olafsson.fridrik@gmail.com>
2017-05-25 14:47:03 +0200

    creating a data sheet for services

---
+++ team-weiland/src/data/tilbyderData.ts
@@ -0,0 +1,88 @@
+﻿export default [
+    {
+        category: 'services',
+        quotes: [
+            {
+                id: '1',
+                service: 'Inspection',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/weiland-08092014-018.jpg',
+                head: 'Ved du ikke hvor vidt det er på tide at få udskiftet dele eller få en reperation? Team Weiland tilbyder inspektion af:',
+                list: ['Rør & stålkonstruktioner', 'Kraner', 'Gittermaster', 'Facader','Tagkonstruktioner','M.M.'],
+                foot: 'Vi har ingen begrænsninger. Kontakt os og vi finder en løsning på opgaven, du ønsker udført.'
+            },
+            {
+                id: '2',
+                service: 'Malararbejde',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/07/IMG_2189.jpg',
+                head: 'Mangler du at få malet din facade? Har du svært ved at nå, eller ønsker en hurtigt og effektiv løsning? Vi tilbyder maling af:',
+                list: ['Facede', 'Vinduer', 'Udhæng', 'M.M.'],
+                foot: 'Vi har ingen begrænsninger på dette område. Kontakt os og vi ser på opgaven, du ønsker udført.'
+            },
+            {
+                id: '3',
+                service: 'Sandblæsning / Epoxymalning',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/weiland-08092014-045.jpeg',
+                head: 'Bange for rust af metaldele? Vi tilbyder overfladebehandling af:',
+                list: ['Rør og stålkonstruktioner', 'Bygninger', 'Broer', 'Industri', 'M.M.'],
+                foot: 'Vores maskiner er kraftige og en sandblæsning/jetcleaning benytter sig af helt op til 3200 bar. Frit stråleblæsning af stålkonstruktioner. Hvis ønskes, kan delene få en gang epoxymaling, for at forhindre fremtidig rust og tæring. Vil tilbyder også opbygning af malingssystemer efter specifikation'
+            },
+            {
+                id: '4',
+                service: 'Fugerarbejde',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/GetAttachment2.jpg',
+                head: 'Har du utætheder i gamle fugninger, eller har du brug for fugearbejde til andre opgaver? Så tilbyder vi fugning af:',
+                list: ['Vinduer','Facader','Div. indlækninger','Reperation / udskiftning','M.M.'],
+                foot: 'Vi har ingen begrænsninger. Kontakt os og vi finder en løsning på opgaven, du ønsker udført.'
+            },
+            {
+                id: '5',
+                service: 'Riggararbejde',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/13902139_314167205587264_79582884_o.jpg',
+                head: 'Har du problemer med flytning af tunge maskiner, rør m.m.? Vi tilbyder Rigger & Anhugger af:',
+                list: ['Rør', 'Broer', 'Maskiner','Stålkonstruktioner','M.M.'],
+                foot: 'Vi tilbyder også opsætning af bygningsstilladser.'
+            },
+            {
+                id: '6',
+                service: 'Trækning af kabler',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/13883942_314165672254084_328667525_n.jpg',
+                head: 'Mange gange kan det være svært at få kablerne placeret på den mest sikre måde. Vi tilbyder derfor:',
+                list: ['Industrimontage af kabler', 'Opsætning af kabelbakker','Montering af lamper i produktion, hvor der er højt til loft.','M.M.'],
+                foot: 'For at kunne yde den bedste service, bliver opgaverne udført af en uddannet Lys & Energitekniker. Vi har LAUST Kursus!'
+            },
+            {
+                id: '7',
+                service: 'Elektrikarbejde',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/foto.jpg',
+                head: 'Mange gange kan det være svært at få elopgaver løst på den mest sikre måde, eller der er mangel på den helt rigtige mand, til løsning af opgaven. Derfor tilbyder vi:',
+                list: ['Industrimontage af kabler', 'Opsætning af kabelbakker','Montering af lamper i produktion, hvor der er højt til loft.','M.M.'],
+                foot: 'For at kunne yde den bedste service, bliver opgaverne udført af en uddannet Lys & Energitekniker. Vi har LAUST Kursus!'
+            },
+            {
+                id: '8',
+                service: 'Tagarbejde',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/GetAttachment3.jpg',
+                head: 'Skal du have dit tag ordnet, for eventuelle utætheder eller farerisikoer? Team Weiland tilbyder:',
+                list: ['Reperation af eksisterende tag', 'Udskiftning af tagsten/tagplader', 'Lægning af tagpap', 'Opsætning af tagrender/nedløbsrør', 'Reperation af udhæng på bygninger','Reperation af inddækning ved skortsten','M.M.'],
+                foot: 'Kontakt os for at forhøre nærmere om hvordan vi bedst ordner dine tagproblemer.'
+            },
+            {
+                id: '9',
+                service: 'Stormskader',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/1492184_10202736750489023_1787267739_o.jpg',
+                head: 'Har du været ude for at blive ramt hårdt af storm og/eller hård blæst? Team Weiland tilbyder:',
+                list: ['Midlertidig afdækning efter storm-/vand-/brandskader', 'Reperation af eksisterende tag', 'Udskiftning af tagsten/tagplader', 'Lægning af tagpap', 'Opsætning af tagrender/nedløbsrør', 'Reperation af udhæng på bygninger','Reperation af inddækning ved skortsten','M.M.'],
+                foot: 'Kontakt os for at forhøre nærmere om hvordan vi bedst ordner dine tagproblemer.'
+            },
+            {
+                id: '10',
+                service: 'Viduespolering / -vask',
+                img: 'http://www.teamweiland.com/wp-content/uploads/2016/08/moykaokon1.jpg',
+                head: 'Denne side vil blive opdateret senere!',
+                list: ['Spolering', 'Vask', 'M.M.'],
+                foot: 'Du kan dog stadigt kontakte os for at forhøre mere omkring vores services indenfor vinduespolering og vask.'
+            },
+        ],
+        icon: 'ios-construct'
+    }
+];
